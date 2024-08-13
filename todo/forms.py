54389706ffb4e6a23ac11dfa843a6fd28617e1d9
TarasFirst <firsttaras@mail.com>
2024-08-13 19:22:05 +0300

    Create todo/forms.py
    - add forms TaskForm, TagForm.

---
+++ todo/forms.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    is_done = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, related_name="tasks")
+
+    def __str__(self):
+        return self.content
+
+    class Meta:
+        ordering = ['is_done', '-created_at']
